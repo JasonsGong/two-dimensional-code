f90a58898d66e71415cee748dfcd21a0517d592f
Jason <2602183349@qq.com>
2023-08-28 23:50:12 +0800

    更改依赖

--- src/main/java/com/mine/code/controller/CodeController.java
+++ src/main/java/com/mine/code/controller/CodeController.java
@@ -5,11 +5,12 @@ import com.google.zxing.EncodeHintType;
 import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.common.BitMatrix;
 import com.google.zxing.qrcode.decoder.ErrorCorrectionLevel;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 
 import javax.imageio.ImageIO;
 import javax.servlet.ServletOutputStream;
